f99e8d11a6725e5bcde674affb338cb5710933b2
abhi <abhi@gmail.com>
2020-05-19 22:01:51 +0530

    Test class completed

---
+++ ICICIPOC/src/com/icici/test/Test.java
@@ -0,0 +1,9 @@
+package com.icici.test;
+
+import com.icici.utils.CustomerUtils;
+
+public class Test {
+	public static void main(String[] args) {
+		System.out.println(CustomerUtils.getCustomer());
+	}
+}
